a0041a7413360ebdcd38e40b40f9a54fc01211f6
Alex Kit <source.kit@gmail.com>
2015-07-20 00:22:09 +0200

    fix (dependency)

--- src/scope-vars.js
+++ src/scope-vars.js
@@ -5,13 +5,6 @@ var io,
 	Class;
 
 (function(){
-	var atma = global;
-	if (atma.Class == null && global.atma)
-		atma = global.atma;
-
-	if (atma.Class == null)
-		atma = require('atma-libs/exports');
-
 
 	Class = global.Class || require('atma-class');
 	Uri = require('atma-utils').class_Uri;
