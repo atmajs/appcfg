e3a6df46ac2134786138e3839bde3afdf10a7182
Alex Kit <source.kit@gmail.com>
2018-11-14 00:28:28 +0100

    fix (build)

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ module.exports = {
 	'settings': {
 		io: {
 			extensions: {
-				js: ['condcomments:read', 'importer:read']
+				js: ['atma-io-middleware-condcomments:read', 'atma-io-middleware-importer:read']
 			}
 		}
 	},
@@ -23,112 +23,12 @@ module.exports = {
 			DEBUG: true
 		}
 	},
-	'jshint': {
-		files: [ 'lib/config.js' ],
-		jshint: JSHint()
-	},
-	'uglify': {
-		files: 'lib/config.js',
-		defines: {
-			DEBUG: false
-		}
-	},
-
+	
 	'watch': {
 		files: 'src/**',
-		config: '#[import]'
+		config: '${import}'
 	},
 	
-	// `> atma bump`
-	'bump': {
-		action: 'custom',
-		script: 'tools/bump.js'
-	},
-
-	'defaults': ['add-handlers', 'import', 'jshint', 'uglify']
+	'defaults': ['add-handlers', 'import']
 };
 
-
-
-
-function JSHint() {
-	
-	var options = {
-			"bitwise": false,
-			"camelcase": false,
-			"curly": false,
-			"eqeqeq": true,
-			"es3": false,
-			"forin": false,
-			"freeze": false,
-			"immed": true,
-			"indent": 2,
-			"latedef": "nofunc",
-			"newcap": false,
-			"noarg": true,
-			"noempty": true,
-			"nonbsp": true,
-			"nonew": false,
-			"plusplus": false,
-			"quotmark": false,
-			"undef": true,
-			"unused": false,
-			"strict": false,
-			"trailing": false,
-			"maxparams": false,
-			"maxdepth": false,
-			"maxstatements": false,
-			"maxcomplexity": false,
-			"maxlen": false,
-			"asi": false,
-			"boss": true,
-			"debug": true,
-			"eqnull": true,
-			"esnext": true,
-			"evil": true,
-			"expr": false,
-			"funcscope": false,
-			"gcl": false,
-			"globalstrict": true,
-			"iterator": false,
-			"lastsemic": true,
-			"laxbreak": true,
-			"laxcomma": true,
-			"loopfunc": false,
-			"maxerr": false,
-			"moz": false,
-			"multistr": true,
-			"notypeof": false,
-			"proto": true,
-			"scripturl": false,
-			"smarttabs": true,
-			"shadow": true,
-			"sub": true,
-			"supernew": true,
-			"validthis": false,
-			"noyield": false,
-			"browser": true,
-			"couch": false,
-			"devel": false,
-			"dojo": false,
-			"jquery": true,
-			"mootools": false,
-			"node": true,
-			"nonstandard": false,
-			"phantom": false,
-			"prototypejs": false,
-			"rhino": false,
-			"worker": false,
-			"wsh": false,
-			"yui": false,
-			"nomen": false,
-			"onevar": false,
-			"passfail": false,
-			"white": false,
-			"predef": ["global", "define", "atma", "io", "net", "mask", "include", "ruta", "ruqq", "Class", "logger", "app", "UTest", "assert", "eq_", "notEq_", "deepEq_", "notDeepEq_", "has_", "hasNot_"]
-		}
-	return {
-		options: options,
-		globals: options.predef
-	};
-}
